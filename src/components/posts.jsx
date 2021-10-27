72a2c36ba03403041a9db083d1eab5914922dc29
Tomas Pesek <pesekt@gmail.com>
2021-10-27 13:27:40 +0200

    props and params

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import queryString from "query-string";
 
-const Posts = () => {
+const Posts = (props) => {
+  const queryParams = queryString.parse(props.location.search);
+  console.log(queryParams);
+
   return (
     <div>
       <h1>Posts</h1>
-      Year: , Month:
+      Year: {props.match.params.year} , Month: {props.match.params.month}
     </div>
   );
 };
